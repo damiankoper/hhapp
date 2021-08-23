404b3c6f723e1f9879a8a75da1db5959c00ba6f5
damiankoper <kopernickk@gmail.com>
2021-08-23 22:16:45 +0200

    feat: watering

--- packages/frontend/components/iot/WateringCan.vue
+++ packages/frontend/components/iot/WateringCan.vue
@@ -13,7 +13,7 @@
           :src="require('~/assets/watering-can.jpg')"
         />
       </v-col>
-      <v-col class="grey lighten-4">
+      <v-col>
         <v-toolbar height="48" elevation="4">
           <v-toolbar-title> {{ status.displayName }}</v-toolbar-title>
           <v-spacer />
@@ -21,75 +21,72 @@
             <template #activator="{ on, attrs }">
               <v-sheet
                 rounded
-                :color="on ? 'green' : 'red'"
+                :color="connected ? 'green' : 'red'"
                 class="pa-1"
                 v-bind="attrs"
                 v-on="on"
               >
                 <v-icon size="1.5em" color="white">
-                  {{ on ? 'mdi-lightbulb-on' : 'mdi-lightbulb-off' }}
+                  {{ connected ? 'mdi-lightbulb-on' : 'mdi-lightbulb-off' }}
                 </v-icon>
               </v-sheet>
             </template>
             <b>Connected!</b> Last update: <br />
-            {{ lastStatusTime }}
+            {{ lastStatusString }}
+          </v-tooltip>
+          <v-tooltip left>
+            <template #activator="{ on, attrs }">
+              <v-sheet
+                rounded
+                color="grey"
+                class="pa-1 ml-2"
+                style="display: inline-block"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon color="white"> mdi-home-search </v-icon>
+              </v-sheet>
+            </template>
+            <b>ID:</b> {{ status.id }} <br />
+            <b>MAC:</b> {{ status.mac }} <br />
+            <b>Type:</b> {{ status.type }}
           </v-tooltip>
         </v-toolbar>
         <v-container>
-          <v-row class="font-weight-bold">
+          <v-row>
             <v-col cols="auto">
-              <v-btn
-                v-ripple
-                color="white"
-                height="100"
-                width="100"
-                elevation="4"
-                @click="emitAction"
-              >
+              <v-btn height="100" width="100" elevation="4" @click="emitAction">
                 <v-icon size="6em" :color="status.enabled ? 'green' : 'red'">
                   mdi-power
                 </v-icon>
                 <br />
               </v-btn>
             </v-col>
-            <v-col>
-              <v-row dense>
-                <v-col :cols="12">
-                  <v-sheet
-                    rounded
+            <v-col cols="auto">
+              <v-sheet
+                rounded
+                class="text-center d-flex flex-column justify-center"
+                color="white"
+                height="100"
+                width="100"
+                elevation="4"
+              >
+                <v-slide-y-transition group leave-absolute>
+                  <v-icon
+                    key="1"
+                    x-large
                     :color="status.pompOn ? 'green' : 'red'"
-                    class="pa-2 mr-1"
-                    style="display: inline-block"
                   >
-                    <v-icon color="white">
-                      {{
-                        status.pompOn ? 'mdi-lightbulb-on' : 'mdi-lightbulb-off'
-                      }}
-                    </v-icon>
-                  </v-sheet>
-                  Pomp on/off
-                </v-col>
-                <v-col :cols="12">
-                  <v-tooltip right>
-                    <template #activator="{ on, attrs }">
-                      <v-sheet
-                        rounded
-                        color="grey"
-                        class="pa-2 mr-1"
-                        style="display: inline-block"
-                        v-bind="attrs"
-                        v-on="on"
-                      >
-                        <v-icon color="white"> mdi-home-search </v-icon>
-                      </v-sheet>
-                    </template>
-                    <b>ID:</b> {{ status.id }} <br />
-                    <b>MAC:</b> {{ status.mac }} <br />
-                    <b>Type:</b> {{ status.type }}
-                  </v-tooltip>
-                  Details
-                </v-col>
-              </v-row>
+                    {{
+                      status.pompOn ? 'mdi-lightbulb-on' : 'mdi-lightbulb-off'
+                    }}
+                  </v-icon>
+                  <div v-if="!disabled" key="2" class="mt-1">
+                    {{ enabledDurationFormat }}
+                  </div>
+                  <div v-if="disabled" key="3" class="mt-1">Pomp off</div>
+                </v-slide-y-transition>
+              </v-sheet>
             </v-col>
           </v-row>
         </v-container>
@@ -99,7 +96,12 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType } from '@nuxtjs/composition-api'
+import {
+  computed,
+  defineComponent,
+  PropType,
+  ref,
+} from '@nuxtjs/composition-api'
 import { DeviceType, IWateringStatus } from 'api-common'
 import { DateTime } from 'luxon'
 export default defineComponent({
@@ -110,18 +112,35 @@ export default defineComponent({
     },
   },
   setup(props, { emit }) {
-    const on = computed(() => {
+    const lastStatus = computed(() => {
+      return DateTime.fromSeconds(props.status.timestamp / 1000)
+    })
+    const connected = computed(() => {
       const timestamp = DateTime.fromSeconds(props.status.timestamp / 1000)
       return timestamp.plus({ minutes: 1 }) > DateTime.now()
     })
-    const lastStatusTime = computed(() => {
-      const timestamp = DateTime.fromSeconds(props.status.timestamp / 1000)
-      return timestamp.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)
+    const lastStatusString = computed(() => {
+      return lastStatus.value.toLocaleString(
+        DateTime.DATETIME_FULL_WITH_SECONDS
+      )
     })
+
+    const enabledTime = ref(DateTime.now())
+    const enabledDuration = computed(() =>
+      lastStatus.value.diff(enabledTime.value)
+    )
     return {
-      on,
-      lastStatusTime,
+      connected,
+      lastStatusString,
+      enabledDuration,
+      enabledDurationFormat: computed(() => {
+        return enabledDuration.value.toFormat('mm:ss')
+      }),
+      disabled: computed(
+        () => !props.status.enabled || enabledDuration.value.as('seconds') < 0
+      ),
       emitAction() {
+        enabledTime.value = DateTime.now()
         emit('action', {
           event: `iot/${DeviceType.WATERING_CAN}/action`,
         })

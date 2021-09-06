8f4c5039c618a16538ddd5c583753ec1c8bbf9b4
damiankoper <kopernickk@gmail.com>
2021-09-06 23:13:04 +0200

    feat: error page

--- packages/frontend/layouts/error.vue
+++ packages/frontend/layouts/error.vue
@@ -1,28 +1,37 @@
 <template>
-  <v-app>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
-  </v-app>
+  <v-row no-gutters justify="center" class="mt-4">
+    <v-col cols="auto">
+      <v-img src="/box.svg" aspect-ratio="1" max-width="100" />
+    </v-col>
+    <v-col cols="12" class="text-center">
+      <div class="text-h4">{{ title }}</div>
+      <div class="text-subtitle-1">Try elsewhere.</div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import { navigationStore } from '~/store'
 export default {
   layout: 'empty',
+  middleware() {
+    navigationStore.setTitle('error')
+  },
   props: {
     error: {
       type: Object,
       default: null,
     },
   },
-  data() {
-    return {
+  setup(props) {
+    const text = {
       pageNotFound: '404 Not Found',
       otherError: 'An error occurred',
+    }
+    return {
+      title:
+        props.error.statusCode === 404 ? text.pageNotFound : text.otherError,
+      ...text,
     }
   },
   head() {
